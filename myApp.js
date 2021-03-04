7679dcedb9ee2cac76109b911ab1b65a600e68bd
rafal4 <rafalplaszczyk.com@gmail.com>
2021-03-04 12:06:53 +0000

    Serve Static Assets

--- myApp.js
+++ myApp.js
@@ -4,6 +4,7 @@ console.log("Hello World")
 console.log(__dirname+"/views/index.html")
 var absolutePath = __dirname+"/views/index.html"
 app.get("/", (req, res) => res.sendFile(absolutePath))
+app.use("/public", express.static(__dirname + "/public"))
 
 
 
