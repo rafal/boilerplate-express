157cc503949cd3a4be0056096236a68939efb339
Rafał Płaszczyk <rafalplaszczyk.com@gmail.com>
2021-03-09 20:39:50 +0000

    Implement a Root-Level Request Logger Middleware

--- myApp.js
+++ myApp.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 var absolutePath = __dirname+"/views/index.html"
+app.use(function middleware(req, res, next) {
+  console.log(req.method + ' ' + req.path + ' - ' + req.ip)
+  next();
+});
 app.get("/json", (req, res) => {
   const msg = "Hello json";
   return res.json({"message": process.env.MESSAGE_STYLE === "uppercase" ? msg.toUpperCase() : msg})})
