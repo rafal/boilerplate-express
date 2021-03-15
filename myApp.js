1b2cd6172f50ccfd490a0915e4c11f047fe19e31
Rafał Płaszczyk <rafalplaszczyk.com@gmail.com>
2021-03-15 09:39:57 +0000

    Get Data from POST Requests

--- myApp.js
+++ myApp.js
@@ -2,13 +2,14 @@ var express = require('express');
 var app = express();
 var absolutePath = __dirname+"/views/index.html"
 var bodyParser = require('body-parser')
-app.get("/name", function(req, res) {
-  var { first: firstName, last: lastName } = req.query;
+app.use(bodyParser.urlencoded({extended: false}));
+app.post("/name", function(req, res) {
+  console.log(JSON.stringify(req.body))
+  var { first: firstName, last: lastName } = req.body;
   res.json({
     name: `${firstName} ${lastName}`
   });
 });
-app.use(bodyParser.urlencoded({extended: false}));
 app.get("/json", (req, res) => {
   const msg = "Hello json";
   return res.json({"message": process.env.MESSAGE_STYLE === "uppercase" ? msg.toUpperCase() : msg})})
